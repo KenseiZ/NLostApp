3a3334ceebd2427706f31bfac6d2a4ed1f220903
KenseiZ <blackdragon18@hotmail.fr>
2018-03-29 18:42:42 +0200

    Add files via upload

--- app/src/main/java/entreprise/nlost/nlostapp/MainActivity.java
+++ app/src/main/java/entreprise/nlost/nlostapp/MainActivity.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Set;
 import java.util.UUID;
+import java.io.*;
 
 import me.aflak.bluetooth.Bluetooth;
 
@@ -35,14 +36,15 @@ public class MainActivity extends AppCompatActivity {
     BluetoothSocket bluetoothSocket = null;
     boolean isConnectedBool = true;
     int newState;
-
+    InputStream tmpIn = null;
+    OutputStream tmpOut = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myHandler = new Handler();
-        myHandler.postDelayed(myRunnable, 10000);
+        myHandler.postDelayed(myRunnable, 5000);
 
     }
 
@@ -57,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         myRunnable = new Runnable() {
             @Override
             public void run() {
+
                 BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
                 //Check si l'utilisateur a activé le bluetooth sinon l'activer
                 if (!bluetoothAdapter.isEnabled()) {
@@ -78,17 +81,21 @@ public class MainActivity extends AppCompatActivity {
                         }
 
                     }
+                     tmpIn = null;
+                     tmpOut = null;
                     Connection();
                     Toast.makeText(MainActivity.this, "Je me connecte", Toast.LENGTH_SHORT).show();
 
                 }
 
-                myHandler.postDelayed(this, 10000);
+                myHandler.postDelayed(this, 5000);
             }
         };
     }
     public void Connection(){
+
         if (bluetoothSocket.isConnected() == false){
+            //bluetoothSocket = null;
             try {
                 bluetoothSocket.connect();
                 Toast.makeText(MainActivity.this, "Je suis Connecté", Toast.LENGTH_SHORT).show();
@@ -104,6 +111,14 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Je Ferme le bluetooth", Toast.LENGTH_SHORT).show();
                 } catch (IOException f) {}
 
+            }
+            try {
+                Toast.makeText(MainActivity.this, "Je lis le temp", Toast.LENGTH_SHORT).show();
+                tmpIn = bluetoothSocket.getInputStream();
+                tmpOut = bluetoothSocket.getOutputStream();
+            }
+            catch (IOException tmpError){
+                NotificationGenerator.OpenActivityNotification(this);
             }
             return;
 
